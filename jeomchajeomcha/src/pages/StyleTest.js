7b3a6980f0721b9323fc4bcd6319c3717d36d217
ParkJiSung <gggg0195@gmail.com>
2023-11-12 21:59:25 +0900

    create theme structure

---
+++ jeomchajeomcha/src/pages/StyleTest.js
@@ -0,0 +1,28 @@
+// StyledComponentsExample.js
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+
+
+// Styled 컴포넌트 정의
+const Header1 = styled.h1`
+  font-family: ${({ theme }) => theme.typography.header1.fontFamily};
+  font-weight: ${({ theme }) => theme.typography.header1.fontWeight};
+  font-size: ${({ theme }) => theme.typography.header1.fontSize};
+  letter-spacing: ${({ theme }) => theme.typography.header1.letterSpacing};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+
+
+// App 컴포넌트
+function StyleTest() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header1>헤더 0 스타일 적용 예시</Header1>
+      <h1>미적용한거</h1>
+    </ThemeProvider>
+  );
+};
+
+export default StyleTest;
