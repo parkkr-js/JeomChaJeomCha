977834674198f5174bfdc7a5d751db63b923f04f
ParkJiSung <gggg0195@gmail.com>
2023-11-12 22:05:29 +0900

    create style example file

--- jeomchajeomcha/src/pages/StyleTest.js
+++ jeomchajeomcha/src/pages/StyleTest.js
@@ -1,21 +1,7 @@
-// StyledComponentsExample.js
-import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { theme } from '../styles/theme';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
 
-
-// Styled 컴포넌트 정의
-const Header1 = styled.h1`
-  font-family: ${({ theme }) => theme.typography.header1.fontFamily};
-  font-weight: ${({ theme }) => theme.typography.header1.fontWeight};
-  font-size: ${({ theme }) => theme.typography.header1.fontSize};
-  letter-spacing: ${({ theme }) => theme.typography.header1.letterSpacing};
-  color: ${({ theme }) => theme.colors.black};
-`;
-
-
-
-// App 컴포넌트
 function StyleTest() {
   return (
     <ThemeProvider theme={theme}>
@@ -23,6 +9,14 @@ function StyleTest() {
       <h1>미적용한거</h1>
     </ThemeProvider>
   );
-};
+}
 
 export default StyleTest;
+
+const Header1 = styled.h1`
+  font-family: ${({ theme }) => theme.typography.header1.fontFamily};
+  font-weight: ${({ theme }) => theme.typography.header1.fontWeight};
+  font-size: ${({ theme }) => theme.typography.header1.fontSize};
+  letter-spacing: ${({ theme }) => theme.typography.header1.letterSpacing};
+  color: ${({ theme }) => theme.colors.black};
+`;
