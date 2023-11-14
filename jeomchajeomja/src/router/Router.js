f66b28548dc5f02eac9c2918a9d174ebbb269e22
ParkJiSung <gggg0195@gmail.com>
2023-11-15 03:52:03 +0900

    각 폴더의 최상위 파일은 index.js로 통일하여 가독성 증가

--- jeomchajeomja/src/router/Router.js
+++ jeomchajeomja/src/router/Router.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "../pages/home";
 import NotFound from "../pages/NotFound";
 import Login from "../pages/Login";
 
