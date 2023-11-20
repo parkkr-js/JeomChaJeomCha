294b957c15e02ed7759e586c82764a076904a33c
ParkJiSung <gggg0195@gmail.com>
2023-11-20 22:50:26 +0900

    create entermemberIngo

---
+++ jeomchajeomja/src/pages/enterMemberInfo/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+function EnterMemberInfo() {
+    return (
+      <Container>
+        <h1>Login</h1>
+      </Container>
+    );
+  } export default EnterMemberInfo;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    `;
--- jeomchajeomja/src/router/Router.js
+++ jeomchajeomja/src/router/Router.js
@@ -3,6 +3,7 @@ import Home from "../pages/home";
 import NotFound from "../pages/NotFound";
 import Login from "../pages/login";
 import SearchResult from "../pages/search";
+import EnterMemberInfo from "../pages/enterMemberInfo";
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
       <Route path="/search" element={<SearchResult />} />
+      <Route path="/enterMemberInfo" element={<EnterMemberInfo />} />
     </Routes>
   );
 }
