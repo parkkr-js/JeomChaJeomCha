4999dfb4dab600f8e22f043af4c2b6db74c04dc6
JinseoKim <eunoia@handong.ac.kr>
2023-11-30 18:12:02 +0900

    구매하기 라우팅

--- jeomchajeomja/src/App.js
+++ jeomchajeomja/src/App.js
@@ -3,15 +3,18 @@ import { theme, GlobalStyle } from "./styles/theme";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
 import SearchProvider from "./model/SearchProvider";
+import PurchaseProvider from "./model/PurchaseProvider";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
-        <SearchProvider>
-          <Router />
-        </SearchProvider>
+        <PurchaseProvider>
+          <SearchProvider>
+            <Router />
+          </SearchProvider>
+        </PurchaseProvider>
       </BrowserRouter>
     </ThemeProvider>
   );
