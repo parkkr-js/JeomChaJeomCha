f3f9a200a0909ef13cbb14e7630dc331aa1290b5
ParkJiSung <gggg0195@gmail.com>
2023-12-03 16:52:44 +0900

    update 주소찾기 모달 수정

--- jeomchajeomja/src/layout/enterMemberInfoPage/components/FindAddressModal.js
+++ jeomchajeomja/src/layout/enterMemberInfoPage/components/FindAddressModal.js
@@ -6,14 +6,23 @@ function FindAddressModal() {
     <Backdrop>
       <ModalContainer>
         <ModalHeaderDiv>
-          <ModalHeader1>주소 찾기</ModalHeader1>
+          <Row1>
+            <ModalHeader1>주소 찾기</ModalHeader1>
+            <ModalBody2>
+              스페이스바를 1초간 누른 후 벨소리가 나면 음성 검색이 활성화됩니다.
+            </ModalBody2>
+          </Row1>
           <ModalHeader2>
             다음과 같은 조항으로 검색을 해서 더욱 정확한 결과가 검색됩니다.
           </ModalHeader2>
-          <ModalBody1>도로명 + 건물번호</ModalBody1>
-          <ModalBody2>예) 판교역로 135</ModalBody2>
-          <ModalBody1> 지역명 + 번지 또는 건물명</ModalBody1>
-          <ModalBody2>예) 남동구아이파크</ModalBody2>
+          <Row2>
+            <ModalBody1>도로명 + 건물번호</ModalBody1>
+            <ModalBody2>예) 삼양로73가길</ModalBody2>
+          </Row2>
+          <Row2>
+            <ModalBody1> 지역명 + 번지 또는 건물명</ModalBody1>
+            <ModalBody2>예) 한빛맹학교</ModalBody2>
+          </Row2>
         </ModalHeaderDiv>
         <InputWrapper>
           <ModalInput placeholder="검색" />
@@ -77,7 +86,8 @@ const ModalHeader2 = styled.div`
   font-size: 25px;
   font-style: normal;
   font-weight: 700;
-  line-height: 37.5px%;
+  line-height: 37.5px;
+  padding-bottom: 10px;
 `;
 
 const ModalBody1 = styled.div`
@@ -89,6 +99,10 @@ const ModalBody1 = styled.div`
 `;
 
 const ModalBody2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
   color: ${({ theme }) => theme.colors.white};
   font-size: 20px;
   font-style: normal;
@@ -132,11 +146,21 @@ const CloseButton = styled.button`
   align-self: center;
 `;
 
-const Row = styled.div`
+const Row1 = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
-  align-items: baseline;
+  align-items: flex-end;
+  width: fit-content;
+  padding-bottom: 20px;
+  gap: 20px;
+`;
+
+const Row2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-end;
   width: fit-content;
   gap: 20px;
 `;
