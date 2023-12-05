099033998097826dcf4c11d04d11fbd7247b4178
ParkJiSung <gggg0195@gmail.com>
2023-12-06 01:04:54 +0900

    address 변수 세팅완료 stt

--- jeomchajeomja/src/layout/enterMemberInfoPage/components/AddressForm.js
+++ jeomchajeomja/src/layout/enterMemberInfoPage/components/AddressForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import { Button } from "../../home/components/Button";
@@ -8,16 +8,99 @@ import { useRecoilValue } from "recoil";
 import { SubAddressState } from "../../../recoil/atoms/AddressState";
 import { useRecoilState } from "recoil";
 import { AddressState } from "../../../recoil/atoms/AddressState";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 function AddressForm() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isListening, setIsListening] = useState(false);
   const address = useRecoilValue(AddressState);
   const [subAddress, setSubAddress] = useRecoilState(SubAddressState);
+  const { transcript } = useSpeechRecognition();
+  const subAddressInputRef = useRef(null);
 
   const handleFindAddressClick = () => {
     setIsModalVisible(!isModalVisible);
   };
+
+  const handleAddressChange = (event) => {
+    if (!isListening) {
+      setSubAddress(event.target.value);
+    }
+  };
+
+  useEffect(() => {
+    let startTimer;
+    const handleKeyDown = (event) => {
+      if (event.key === " " && !isListening && !startTimer) {
+        startTimer = setTimeout(() => {
+          playBeep();
+          setIsListening(true);
+          SpeechRecognition.startListening({
+            language: "ko-KR",
+          });
+          startTimer = null;
+        }, 200);
+      }
+    };
+
+    const handleKeyUp = (event) => {
+      if (event.key === " " && isListening) {
+        if (startTimer) {
+          clearTimeout(startTimer);
+          startTimer = null;
+        }
+        if (isListening) {
+          setIsListening(false);
+          SpeechRecognition.stopListening();
+        }
+      }
+    };
+
+    const playBeep = () => {
+      const audioContext = new window.AudioContext();
+      const oscillator = audioContext.createOscillator();
+      const gainNode = audioContext.createGain();
+      gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
+      oscillator.type = "sine";
+      oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
+      oscillator.connect(gainNode);
+      gainNode.connect(audioContext.destination);
+      oscillator.start();
+      oscillator.stop(audioContext.currentTime + 0.6);
+    };
+
+    if (document) {
+      document.addEventListener("keydown", handleKeyDown);
+      document.addEventListener("keyup", handleKeyUp);
+    }
+
+    return () => {
+      if (document) {
+        document.removeEventListener("keydown", handleKeyDown);
+        document.removeEventListener("keyup", handleKeyUp);
+      }
+      if (startTimer) {
+        clearTimeout(startTimer);
+      }
+    };
+  }, [isListening, subAddressInputRef]);
+
+  useEffect(() => {
+    if (isListening && transcript) {
+      setSubAddress(transcript);
+    }
+  }, [transcript, isListening]);
+
+  useEffect(() => {
+    if (subAddress && transcript && !isListening) {
+      const speech = new SpeechSynthesisUtterance();
+      speech.lang = "ko-KR";
+      speech.text = subAddress;
+      window.speechSynthesis.speak(speech);
+    }
+  }, [isListening]);
 
   return (
     <Colunm>
@@ -80,10 +163,12 @@ function AddressForm() {
           border: "2px solid #000",
           boxShadow: "0px 6px 10px 0px rgba(0, 0, 0, 0.10)",
           overflow: "hidden",
-          backgroundColor: "transparent",
+          backgroundColor: isListening ? "red" : "transparent",
         }}
       >
         <InputBase
+          ref={subAddressInputRef}
+          onChange={handleAddressChange}
           value={subAddress}
           sx={{
             ml: 1,
